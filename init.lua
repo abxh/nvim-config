a05b378500acf70f6feee0ce6707da26c7b8f8d8
abxh <83485102+abxh@users.noreply.github.com>
2025-01-05 20:49:38 +0100

    fix init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
 vim.loader.enable() -- experimental neovim feature; cache neovim modules
 
+require("autocmds")
+vim.cmd("source " .. vim.fn.stdpath("config") .. "/lua/autocmds.vim")
+
 local o = vim.opt
 
 o.wrap = false
@@ -29,3 +32,27 @@ o.viewdir = "/tmp/view//"
 
 o.updatetime = 50
 o.isfname:append("@-@")
+
+local keymaps = require("keymaps")
+if keymaps.leaderkey ~= nil then
+  vim.keymap.set("", keymaps.leaderkey, "<Nop>", keymaps.default_opts)
+  vim.g.mapleader = keymaps.leaderkey
+  vim.g.maplocalleader = keymaps.leaderkey
+end
+for _, value in pairs(keymaps.core) do
+  vim.keymap.set(unpack(value))
+end
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup(unpack(require("plugins")))
